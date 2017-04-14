7f9aba59a974884e5d857dc0aff5039d0cef1e40
Agustina Barbetta <agustina.barbetta@gmail.com>
2017-04-14 02:37:37 -0300

    Rename client

---
+++ src/app/app.config.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var AppConfig = (function () {
+    function AppConfig() {
+        this.apiUrl = 'http://localhost:3000/api';
+    }
+    return AppConfig;
+}());
+exports.AppConfig = AppConfig;
+;
+//# sourceMappingURL=app.config.js.map
